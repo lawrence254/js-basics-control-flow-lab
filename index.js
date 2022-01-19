f06b97acb7da1042c03efdb834572dbe128d8368
lawrence254 <lawrenslava1@gmail.com>
2022-01-19 16:55:25 +0300

    Complete implementation of all tests.
    All tests are passing

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(input) {
   // Write your code here!
+  if (input <= 400) {
+    return 'This one is on me!';
+  } else if (input > 2500) {
+    return 'No can do.'
+  }
+  else if (input > 2000) {
+    return 'I will gladly take your thirty bucks.';
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city) {
   // Write your code here!
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(charm) {
   // Write your code here!
+  switch (charm) {
+    case 'generous':
+      return 'Thank you so much.'
+      break;
+    case 'not as generous':
+      return 'Thank you.'
+      break;
+
+    default:
+      return 'Bye.'
+      break;
+  }
 }
